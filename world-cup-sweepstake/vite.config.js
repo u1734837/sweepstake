457e486a3635e87006e10ef370069e96d610773a
Nathan Burchell <u1734837@uel.ac.uk>
2026-06-09 15:15:55 +0100

    add auto deploy workflow

--- world-cup-sweepstake/vite.config.js
+++ world-cup-sweepstake/vite.config.js
@@ -3,7 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-
-  // IMPORTANT: must match your GitHub repo name
-  base: "/sweepstake/"
+  base: "/sweepstake/",
 });
